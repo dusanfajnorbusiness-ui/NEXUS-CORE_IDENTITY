d1a98adbfa241ae230bd1924b2ed37a777e57133
Dušan Fajnor <tvoj@email.com>
2026-02-10 13:17:37 +0100

    update: dimensions IDs

--- js/dimensions/id02.js
+++ js/dimensions/id02.js
@@ -1,3 +1,12 @@
-window.id02 = {
- proContent: `<div class="p-4 opacity-50 italic text-[10px]">Dáta pre dimenziu 02 sa načítavajú z neuronálnej siete...</div>`
+window.id02Data = {
+  proContent: `
+    <div class="p-6 border border-dashed border-white/10 rounded-lg text-center animate-pulse">
+      <p class="text-[10px] font-mono text-white/40 uppercase tracking-[0.3em]">
+        [ SYNCHRONIZING_DATA_NODE_02 ]
+      </p>
+      <p class="text-[8px] font-mono text-[#39FF14]/40 mt-2 italic">
+        Čakám na vloženie ZorbaBudha Protocol dokumentácie...
+      </p>
+    </div>
+  `
 };
